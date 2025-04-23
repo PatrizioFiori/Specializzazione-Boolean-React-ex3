396ba810d71a66ce99b2821a94d65d932b9cb4d9
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-04-23 16:45:05 +0200

    Add milestone description for data retrieval and display

--- src/App.jsx
+++ src/App.jsx
@@ -7,3 +7,25 @@ const App = () => {
 }
 
 export default App
+
+
+
+/*
+
+📌 Milestone 1: Recuperare e visualizzare i dati
+Effettua una chiamata API a
+https://boolean-spec-frontend.vercel.app/freetestapi/politicians
+Salva la risposta in uno stato React (useState).
+
+Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
+
+Nome (name)
+Immagine (image)
+Posizione (position)
+Breve biografia (biography)
+
+Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
+
+
+
+*/
